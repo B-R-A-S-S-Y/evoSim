725a85bf1c7ff112cdb2de75fb4799d8ee6de320
B-r-a-s-s-y <daniel.c.dey@live.com>
2014-10-06 14:46:03 +1300

    Added Events and Player Organism

--- evoSim-C/Program.cs
+++ evoSim-C/Program.cs
@@ -12,13 +12,17 @@ namespace evoSim_C
         {
             Generation.Organism[] orgsList = Generation.GenPhase(64);
             int roundNum = 1;
-            while (orgsList.Length > 2)
+            Console.Write("Select your organism: ");
+            int playerOrg = int.Parse(Console.ReadLine());
+            orgsList[playerOrg].oName = orgsList[playerOrg].oName.ToUpper();
+            while (orgsList.Length > 2 && orgsList[playerOrg].oHealth >= 0)
             {
                 Console.WriteLine("Round {0}!", roundNum);
                 orgsList = Combat.Init(orgsList);
                 roundNum++;
                 Console.ReadKey();
                 Console.Clear();
+                Event.eventCall(orgsList[playerOrg]);
             }
             Console.WriteLine("FINAL ROUND: {0} VS {1}", orgsList[0].oName, orgsList[1].oName);
             Combat.Init(orgsList);
